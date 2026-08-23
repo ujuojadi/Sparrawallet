32d304c47e542c1e22b7109c5c496392d026206a
Obianuju Ojadi <88194962+ujuojadi@users.noreply.github.com>
2026-08-23 17:35:27 -0500

    Update authService.js

--- Wallet-management-frontend/src/features/auth/authService.js
+++ Wallet-management-frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/accounts/";
+const API_URL = `${process.env.REACT_APP_API_URL}/accounts/`;
 
 //Register new user
 const register = async (userData) => {
